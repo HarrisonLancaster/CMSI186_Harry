da448e2c7fffc7aa997731f3ac4075e438791a22
HarrisonLancaster <46763883+HarrisonLancaster@users.noreply.github.com>
2019-02-01 13:57:26 -0800

    Added CalendarStuff.java and CountTheDays.java

---
+++ homework01/CountTheDays.java
@@ -0,0 +1,20 @@
+public class CountTheDays {
+
+   public static void main(String[]args){
+      long month0 = (Long.parseLong(args[0]));
+      long day0 = (Long.parseLong(args[1]));
+      long year0 = (Long.parseLong(args[2]));
+      long month1 = (Long.parseLong(args[3]));
+      long day1 = (Long.parseLong(args[4]));
+      long year1 = (Long.parseLong(args[5]));
+
+      System.out.println(CalendarStuff.daysBetween(month0,day0,year0,month1,day1,year1));
+
+      try{
+         CalendarStuff.isValidDate(month0 ,day0, year0);
+         CalendarStuff.isValidDate(month1, day1, year1);
+      } catch (NumberFormatException e) {
+         System.out.println("Enter a valid date.");
+      }
+   }
+}
